9adbd94cdc1cfd6466f7b4748bd5da802963de3e
graph1589 <graph1589@gmail.com>
2019-10-03 18:53:07 +0300

    fix magic number

--- src/game-engine.js
+++ src/game-engine.js
@@ -1,12 +1,14 @@
 import readlineSync from 'readline-sync';
 
+const sumOfRound = 3;
+
 export default (game, description) => {
   console.log('\nWelcome to the Brain games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
 
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < sumOfRound; i += 1) {
     const message = game();
     const question = message[0];
     const rightAnswer = message[1];
