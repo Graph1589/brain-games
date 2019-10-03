fc857f3a1246d9cea3c2ba2e52f4de1121dea673
graph1589 <graph1589@gmail.com>
2019-10-03 19:09:49 +0300

    function name changed

--- src/game-engine.js
+++ src/game-engine.js
@@ -2,14 +2,14 @@ import readlineSync from 'readline-sync';
 
 const sumOfRound = 3;
 
-export default (game, description) => {
+export default (play, description) => {
   console.log('\nWelcome to the Brain games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
 
   for (let i = 0; i < sumOfRound; i += 1) {
-    const message = game();
+    const message = play();
     const question = message[0];
     const rightAnswer = message[1];
     console.log(`Question: ${question}`);
