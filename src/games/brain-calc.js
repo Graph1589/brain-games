13788536f5fdc97664f9dda39fde069756e0244f
graph1589 <graph1589@gmail.com>
2019-10-17 20:11:57 +0300

    fix const name

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,7 +2,7 @@ import getRandomInteger from '../utilities/randomizer';
 import engine from '../game-engine';
 
 const operators = ['+', '-', '*'];
-const maxValueOfNumbers = 35;
+const maxValue = 35;
 
 const getRightAnswer = (firstNumber, operator, secondNumber) => {
   let rightAnswer = 0;
@@ -22,8 +22,8 @@ const getRightAnswer = (firstNumber, operator, secondNumber) => {
 };
 
 const brainCalc = () => {
-  const firstNumber = getRandomInteger(maxValueOfNumbers);
-  const secondNumber = getRandomInteger(maxValueOfNumbers);
+  const firstNumber = getRandomInteger(maxValue);
+  const secondNumber = getRandomInteger(maxValue);
   const operator = operators[getRandomInteger(operators.length, 0)];
   const question = `${firstNumber} ${operator} ${secondNumber}`;
   const rightAnswer = getRightAnswer(firstNumber, operator, secondNumber);
